4be8209fa6ae0341f265dc3fdd7be8aee7ac0894
Lester López <llopez-2022064@kinal.edu.gt>
2024-09-24 15:29:08 -0600

    adding liquibase and swagger core

---
+++ src/test/java/com/stackoverflow/stackoverflow/StackoverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackoverflow.stackoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackoverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
